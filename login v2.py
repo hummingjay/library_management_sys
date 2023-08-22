b486a7ada8c61901dbaa1015d9aaa6fd7bd17b54
hummingjay <jays7mill@gmail.com>
2023-08-22 12:48:55 +0300

    Two login versions

---
+++ login v2.py
@@ -0,0 +1,68 @@
+from tkinter import *
+import customtkinter
+import mysql.connector
+
+class inputframe(customtkinter.CTkFrame):
+    """Sets up up the frame for the login"""
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.label =customtkinter.CTkLabel(self, text="User Login", font=("Helvetica", 28))
+        self.label.grid(row=0, column=0, pady=12, padx=10)
+        
+        # inputs
+        self.user = customtkinter.CTkEntry(self, placeholder_text="Username")
+        self.user.grid(row=1, column=0, pady=12, padx=10)
+        self.passwd = customtkinter.CTkEntry(self, placeholder_text="Password", show="*")
+        self.passwd.grid(row=2,column=0, pady=12, padx=10)
+        
+        #login button
+        self.button = customtkinter.CTkButton(self, text="Login", command=App.login)
+        self.button.grid(row=3, column=0, pady=12, padx=10)
+        
+        self.checkbox = customtkinter.CTkCheckBox(self, text="Remember Me")
+        self.checkbox.grid(row=4, column=0, pady=12, padx=10)
+    
+class App(customtkinter.CTk):
+    """Main app for running login page"""
+    def __init__(self):
+        super().__init__()
+        
+        self.title("Login Page")
+        self.geometry("600x400")
+        self.iconbitmap("login.ico")
+        
+        customtkinter.set_appearance_mode("system")
+        customtkinter.set_default_color_theme("blue")
+        
+    
+        #input call
+        self.inputframe = inputframe(self)
+        self.inputframe.pack(side = LEFT, fill=BOTH)
+        
+    def login(self):
+        """ Loging the user into the system"""
+        username = self.user.get()
+        password = self.passwd.get()
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="password",
+            database="my_database"
+        )
+        cursor = connection.cursor()
+        
+        cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s")
+        
+        results = cursor.fetchall()
+        
+        if len(results) == 0:
+            print("Invalid username or password")
+        else:
+            print("Login successful")
+        
+        cursor.close()
+        connection.close()
+
+app = App()
+app.mainloop()
---
+++ login_page.py
@@ -0,0 +1,64 @@
+import customtkinter
+import mysql.connector
+
+"""
+    This is the login page for the library management sytem
+"""
+
+#setting appearance and theme
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+#the appliction 
+root = customtkinter.CTk()
+root.geometry("600x400")
+root.title("Login page")
+
+#icon in title bar
+root.iconbitmap('login.ico')
+
+def login():
+    """Logs the user into the system"""
+    username = entry1.get()
+    password = entry2.get()
+    
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="my_database"
+    )
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    
+    results = cursor.fetchall()
+    
+    if len(results) == 0:
+        print("Invalid username or passsword.")
+    else:
+        print("Login successful.")
+    
+    cursor.close()
+    connection.close()
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="User Login", font=("Helvetica", 28))
+label.pack(pady=12, padx=10)
+
+#inputs
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
