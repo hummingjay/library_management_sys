4ba9408084692b22becc6e400a1ebcc0b1639e49
hummingjay <jays7mill@gmail.com>
2023-08-28 00:25:48 +0300

    Finally full side bar

--- Main.py
+++ Main.py
@@ -2,28 +2,34 @@ import customtkinter
 #import loginv2
 from tkinter import *
 
-class leftframe(customtkinter.CTkFrame):
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+#'''
+class sideframe(customtkinter.CTkFrame):
     def __init__(self, master):
-        super().__init__(master)
-        
-
-class topframe(customtkinter.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-        
-        
+        super().__init__(master, width=140, corner_radius=7)
+#'''        
 class App(customtkinter.CTk):
     """Main instance of the App"""
     def __init__(self):
         super().__init__()
         
-        customtkinter.set_default_color_theme("blue")
+        self.title("Library Management system")
+        self.geometry("770x700")
         
-        self.leftframe = leftframe(self)
-        self.leftframe.pack(side=LEFT)
+        # configuring the grid layout
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2,3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
         
-        self.topframe = topframe(self)
-        self.topframe.pack(side=TOP)
+        #self.sideframe =customtkinter.CTkFrame(self, width=140, corner_radius=7)
+        self.sideframe = sideframe(self)
+        self.sideframe.grid(row=0, rowspan=4, column=0, sticky="wens")
+        self.grid_rowconfigure(3, weight=1)
         
-app = App()
-app.mainloop()
+
+        
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
