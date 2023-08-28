a5dbb5c8e0fe1e4e824c2146e2711734480ca88e
hummingjay <jays7mill@gmail.com>
2023-08-28 10:10:06 +0300

    Update to Main.py

--- Main.py
+++ Main.py
@@ -4,11 +4,13 @@ from tkinter import *
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
-#'''
+
 class sideframe(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master, width=140, corner_radius=7)
-#'''        
+        self.the_label = customtkinter.CTkLabel(self, text="Management", font=("Helvetica", 14))
+        self.the_label.grid(row=0, column=0, padx=20, pady=10)
+      
 class App(customtkinter.CTk):
     """Main instance of the App"""
     def __init__(self):
@@ -22,7 +24,7 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2,3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
         
-        #self.sideframe =customtkinter.CTkFrame(self, width=140, corner_radius=7)
+        # calling frames
         self.sideframe = sideframe(self)
         self.sideframe.grid(row=0, rowspan=4, column=0, sticky="wens")
         self.grid_rowconfigure(3, weight=1)
