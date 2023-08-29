b56ef8d12782ad2616724d2813daf1eaf526d743
hummingjay <jays7mill@gmail.com>
2023-08-29 06:58:07 +0300

    Buttoned

--- Main.py
+++ Main.py
@@ -8,8 +8,17 @@ customtkinter.set_default_color_theme("blue")
 class sideframe(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master, width=140, corner_radius=7)
-        self.the_label = customtkinter.CTkLabel(self, text="Management", font=("Helvetica", 14))
+        self.the_label = customtkinter.CTkLabel(self, text="My Library", font=("Helvetica", 14))
         self.the_label.grid(row=0, column=0, padx=20, pady=10)
+        
+        self.searchbtn = customtkinter.CTkButton(self, text="Catalog", command=App.books_view)
+        self.searchbtn.grid(row=2, column=0, padx=10, pady=20)
+        
+        self.adduserbtn =customtkinter.CTkButton(self, text="Add User", command=App.add_user)
+        self.adduserbtn.grid(row=3, column=0, padx=20, pady=10)
+        
+        self.circulate = customtkinter.CTkButton(self, text="Circulation", command=App.circulation)
+        self.circulate.grid(row=4, column=0, padx=20, pady=10)
       
 class App(customtkinter.CTk):
     """Main instance of the App"""
@@ -28,10 +37,16 @@ class App(customtkinter.CTk):
         self.sideframe = sideframe(self)
         self.sideframe.grid(row=0, rowspan=4, column=0, sticky="wens")
         self.grid_rowconfigure(3, weight=1)
+    
+    def books_view(self):
+        pass
+    
+    def add_user(self):
+        pass
+    
+    def circulation(self):
+        pass
         
-
-        
-
 if __name__ == "__main__":
     app = App()
     app.mainloop()
