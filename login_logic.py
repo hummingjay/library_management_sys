dc1d3c29291f45955d063fc7ccdde1216e60c9e3
hummingjay <jays7mill@gmail.com>
2023-09-04 23:01:38 +0300

    RAdical functioning change

---
+++ login_logic.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter.messagebox
+import sqlite3
+
+
+class login_logic:
+    def create_new_user(self):
+        while True:
+            tkinter.messagebox.showerror("User name", "User name exists")
+    
+    def database_check(self):
+          
+    def login(self, username, password):
+        
